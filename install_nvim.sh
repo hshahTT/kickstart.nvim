69995b297d47a72ddcf248035d72df0416b28490
Het Shah <hshah@tenstorrent.com>
2026-06-15 21:59:41 +0000

    add nvim installation script

---
+++ install_nvim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ~
+rm nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+if ! grep -Fxq 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' ~/.bashrc; then
+    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+fi
+sudo apt install ripgrep fd-find unzip xclip -y
+rm tree-sitter-cli-linux-x64.zip
+wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.26.7/tree-sitter-cli-linux-x64.zip
+unzip tree-sitter-cli-linux-x64.zip
+chmod +x tree-sitter
+sudo mv tree-sitter /opt/nvim-linux-x86_64/bin
+
+# lazygit
+rm -f lazygit.tar.gz lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+chmod +x lazygit
+sudo mv lazygit /opt/nvim-linux-x86_64/bin
+rm -f lazygit.tar.gz
+
